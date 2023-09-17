d1cf2ceddd9b33120948f05ec9af4dcf11ae6cdd
ojanojan1453 <144782320+ojanojan1453@users.noreply.github.com>
2023-09-17 21:55:12 +0800

    Add files via upload

---
+++ PRAK002-2310817210021-Rakhmad Fauzhan Ramadhan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char *time[] = {"Pagi", "Siang", "Malam"};
+    
+    for (int i = 0; i < 3; i++) {
+        printf("Selamat %s, Rakhmad Fauzhan Ramadhan\n", time[i]);
+    }
+    
+    return 0;
+}
